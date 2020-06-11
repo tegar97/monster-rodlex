59dcccac5ab933fd50cc235ee0ea8827fe4f5c28
tegar97 <eategar@gmail.com>
2020-06-11 12:11:39 +0700

    Updates

---
+++ precache-manifest.8621cd52e647ae9d8dca86331ce73f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113eeb369ef33ea057abe4f565a091fa",
+    "url": "/monster-rodlex/index.html"
+  },
+  {
+    "revision": "59bfa856c78c32d46a15",
+    "url": "/monster-rodlex/static/css/main.c259552b.chunk.css"
+  },
+  {
+    "revision": "30d9bbb2df6415e8bfa2",
+    "url": "/monster-rodlex/static/js/2.c35fc075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rodlex/static/js/2.c35fc075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bfa856c78c32d46a15",
+    "url": "/monster-rodlex/static/js/main.95cbfc51.chunk.js"
+  },
+  {
+    "revision": "b17f0f078c77343f92a7",
+    "url": "/monster-rodlex/static/js/runtime-main.89102d29.js"
+  }
+]);
